392c6713b3647032bae6b373aa5f6949920ca27d
Chris Friday <chrisfriday033@gmail.com>
2025-11-13 07:48:04 +0100

    added redirect

--- src/utilities/Trade.jsx
+++ src/utilities/Trade.jsx
@@ -1,109 +1,112 @@
 // import { useState } from "react";
 
 import { Spinner } from "flowbite-react";
+import { useNavigate } from "react-router-dom";
 
 const Trade = ({
-  setActiveTab,
-  activeTab,
-  setTradeForm,
-  createTrade,
-  buyLoading,
-  sellLoading,
-  tradeForm,
+    setActiveTab,
+    activeTab,
+    setTradeForm,
+    createTrade,
+    buyLoading,
+    sellLoading,
+    tradeForm,
 }) => {
-  return (
-    <div className="flex flex-col gap-y-6 w-full px-2 md:px-3 lg:px-8 xl:px-12 py-2">
-      <h1 className="text-xl font-bold text-white">Trade</h1>
-      {/* trade buttons */}
-      <div className="px-2 md:px-3 lg:px-8 xl:px-12 flex flex-col w-full lg:flex-row items-center justify-between">
-        {/* buy/sell  */}
-        <div className="flex  w-full items-center justify-between gap-x-3 lg:justify-normal lg:gap-x-8 lg:w-[60%]">
-          {buyLoading ? (
-            <span className="min-w-[100px] w-full lg:w-[200px] flex items-center justify-center">
-              <Spinner color="warning" size="lg" />
-            </span>
-          ) : (
-            <button
-              onClick={(e) => {
-                e.preventDefault();
-                e.stopPropagation();
 
-                const trade_type = "buy";
-                const newForm = { ...tradeForm, trade_type }; // use your existing tradeForm state here
-                setTradeForm(newForm);
-                createTrade(newForm);
-              }}
-              className="bg-green-500 min-w-[100px] w-full lg:w-[200px] py-3 text-sm  rounded-lg font-semibold text-white"
-            >
-              Buy
-            </button>
-          )}
-          {sellLoading ? (
-            <span className="min-w-[100px] w-full lg:w-[200px] flex items-center justify-center">
-              <Spinner color="warning" size="lg" />
-            </span>
-          ) : (
-            <button
-              onClick={(e) => {
-                e.preventDefault();
-                e.stopPropagation();
+    const navigate = useNavigate()
+    return (
+        <div className="flex flex-col gap-y-6 w-full px-2 md:px-3 lg:px-8 xl:px-12 py-2">
+            <h1 className="text-xl font-bold text-white">Trade</h1>
+            {/* trade buttons */}
+            <div className="px-2 md:px-3 lg:px-8 xl:px-12 flex flex-col w-full lg:flex-row items-center justify-between">
+                {/* buy/sell  */}
+                <div className="flex  w-full items-center justify-between gap-x-3 lg:justify-normal lg:gap-x-8 lg:w-[60%]">
+                    {buyLoading ? (
+                        <span className="min-w-[100px] w-full lg:w-[200px] flex items-center justify-center">
+                            <Spinner color="warning" size="lg" />
+                        </span>
+                    ) : (
+                        <button
+                            onClick={(e) => {
+                                e.preventDefault();
+                                e.stopPropagation();
 
-                const trade_type = "sell";
-                const newForm = { ...tradeForm, trade_type }; // use your existing tradeForm state here
-                setTradeForm(newForm);
-                createTrade(newForm);
-              }}
-              className="bg-red-500 min-w-[100px] w-full lg:w-[200px] text-sm py-3  rounded-lg font-semibold text-white"
-            >
-              Sell
-            </button>
-          )}
-          <button className="hidden lg:flex min-w-[100px] w-full bg-primary lg:w-[200px] text-black font-semibold text-sm rounded-lg py-3 items-center justify-center text-center">
-            Copy trade
-          </button>
+                                const trade_type = "buy";
+                                const newForm = { ...tradeForm, trade_type }; // use your existing tradeForm state here
+                                setTradeForm(newForm);
+                                createTrade(newForm);
+                            }}
+                            className="bg-green-500 min-w-[100px] w-full lg:w-[200px] py-3 text-sm  rounded-lg font-semibold text-white"
+                        >
+                            Buy
+                        </button>
+                    )}
+                    {sellLoading ? (
+                        <span className="min-w-[100px] w-full lg:w-[200px] flex items-center justify-center">
+                            <Spinner color="warning" size="lg" />
+                        </span>
+                    ) : (
+                        <button
+                            onClick={(e) => {
+                                e.preventDefault();
+                                e.stopPropagation();
+
+                                const trade_type = "sell";
+                                const newForm = { ...tradeForm, trade_type }; // use your existing tradeForm state here
+                                setTradeForm(newForm);
+                                createTrade(newForm);
+                            }}
+                            className="bg-red-500 min-w-[100px] w-full lg:w-[200px] text-sm py-3  rounded-lg font-semibold text-white"
+                        >
+                            Sell
+                        </button>
+                    )}
+                    <button onClick={() => navigate('/market')} className="hidden lg:flex min-w-[100px] w-full bg-primary lg:w-[200px] text-black font-semibold text-sm rounded-lg py-3 items-center justify-center text-center">
+                        Copy trade
+                    </button>
+                </div>
+
+                {/*  trade options button */}
+                <div className="hidden lg:flex w-full bg-whyCard rounded-lg p-0 mt-3 lg:mt-0 lg:w-[20%]">
+                    <button
+                        onClick={() => setActiveTab("stock")}
+                        className={`${
+                            activeTab === "stock"
+                                ? "bg-primary text-black"
+                                : "bg-whyCard text-white"
+                        }font-semibold text-sm py-3 w-full rounded-lg text-white cursor-pointer hover:transition-transform hover:border-1 hover:border-gray-600 duration-100 ease-in-out`}
+                    >
+                        Stock
+                    </button>
+                    <button
+                        onClick={() => setActiveTab("crypto")}
+                        className={`${
+                            activeTab === "crypto"
+                                ? "bg-primary text-black"
+                                : "bg-whyCard text-white"
+                        }font-semibold text-sm py-3 w-full rounded-lg text-white cursor-pointer hover:transition-transform hover:border-1 hover:border-gray-600 duration-100 ease-in-out`}
+                    >
+                        Crypto
+                    </button>
+                    <button
+                        onClick={() => setActiveTab("forex")}
+                        className={`${
+                            activeTab === "forex"
+                                ? "bg-primary text-black"
+                                : "bg-whyCard text-white"
+                        }font-semibold text-sm py-3 w-full rounded-lg text-white cursor-pointer hover:transition-transform hover:border-1 hover:border-gray-600 duration-100 ease-in-out`}
+                    >
+                        Forex
+                    </button>
+                </div>
+
+                {/* mobile copy trade button */}
+                <button className="lg:hidden min-w-[100px] w-full bg-primary lg:w-[200px] text-black font-semibold text-sm rounded-lg py-3 items-center justify-center text-center mt-2">
+                    Copy trade
+                </button>
+            </div>
         </div>
-
-        {/*  trade options button */}
-        <div className="hidden lg:flex w-full bg-whyCard rounded-lg p-0 mt-3 lg:mt-0 lg:w-[20%]">
-          <button
-            onClick={() => setActiveTab("stock")}
-            className={`${
-              activeTab === "stock"
-                ? "bg-primary text-black"
-                : "bg-whyCard text-white"
-            }font-semibold text-sm py-3 w-full rounded-lg text-white cursor-pointer hover:transition-transform hover:border-1 hover:border-gray-600 duration-100 ease-in-out`}
-          >
-            Stock
-          </button>
-          <button
-            onClick={() => setActiveTab("crypto")}
-            className={`${
-              activeTab === "crypto"
-                ? "bg-primary text-black"
-                : "bg-whyCard text-white"
-            }font-semibold text-sm py-3 w-full rounded-lg text-white cursor-pointer hover:transition-transform hover:border-1 hover:border-gray-600 duration-100 ease-in-out`}
-          >
-            Crypto
-          </button>
-          <button
-            onClick={() => setActiveTab("forex")}
-            className={`${
-              activeTab === "forex"
-                ? "bg-primary text-black"
-                : "bg-whyCard text-white"
-            }font-semibold text-sm py-3 w-full rounded-lg text-white cursor-pointer hover:transition-transform hover:border-1 hover:border-gray-600 duration-100 ease-in-out`}
-          >
-            Forex
-          </button>
-        </div>
-
-        {/* mobile copy trade button */}
-        <button className="lg:hidden min-w-[100px] w-full bg-primary lg:w-[200px] text-black font-semibold text-sm rounded-lg py-3 items-center justify-center text-center mt-2">
-          Copy trade
-        </button>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Trade;
